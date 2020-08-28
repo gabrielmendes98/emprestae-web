2c9e27093cb293cc396c08de2ace987d7c740d2f
Gabriel <gabrielmssantiago@gmail.com>
2020-08-28 02:26:01 -0300

    tela de mudar senha pronta

---
+++ src/pages/ChangePassword/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container } from './styles';
+
+const ChangePassword: React.FC<RouteComponentProps> = ({ history }) => {
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    // senhas (formato e igualdade)
+    // se estiver errado, mostra toast de erro
+
+    // http request pro backend pedindo troca de senha com aqueles dados
+    // se der tudo certo, redireciona para o perfil
+    // se nao, manda um toast de erro
+
+    history.push('/user/12345');
+  }
+
+  return (
+    <Container>
+      <h1>Emprestaê</h1>
+      <div>
+        <Link to="/user/12345">
+          <FiArrowLeft />
+        </Link>
+        <h2>Mudar senha</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <Input placeholder="Digite sua senha atual" />
+          <Input placeholder="Digite sua nova senha" />
+          <Input placeholder="Confirmar nova senha" />
+          <Button>Cadastrar</Button>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default ChangePassword;
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import ChangePassword from './pages/ChangePassword';
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/sign-up" component={SignUp} />
+      <Route path="/change-password" component={ChangePassword} />
     </Switch>
   );
 };
